d2011dcae0cf9d1a551af31e0ff06e1ffe3a4750
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-12 22:23:17 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(input, array)
+def input_to_index(input)
   input.to_i = array
   array -= 1
   return array
